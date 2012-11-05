52477d6515041caed06cc3d71ba73e82fb667d00
oumallouk rachid <rachid_ouma@live.fr>
2012-11-05 14:33:27 +0000

    first test javaee and php

---
+++ src/main/webapp/views/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    	               "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+<title >Premier projet Java EE avec PHP</title >
+<?php
+	function quercus_test()
+	{
+		return function_exists ("quercus_version");
+	}
+?>
+</head>
+<body>
+<p>
+Test de Quercus
+</p>
+<?php
+	if (quercus_test())
+	{
+		echo  "Quercus fonctionne parfaitement";
+	}
+?>
+<p>
+Test instance de la class personne
+</p>
+<?php
+	//importation du paquetage Java
+	import  com.javaee.php.entites.Personne;
+	//instanciation de la classe
+	$pojo=new Personne();
+	//declenchement de methodes
+	$pojo->setId(1);
+	$pojo->setNom("Lafosse");
+	echo  $pojo->afficherPersonne();
+	//importation
+	import  java.util.Calendar;
+	//singleton
+	$calendar=Calendar::getInstance();
+	//afficher la valeur
+	echo ("<br/>".$calendar);
+?>
+</body>
+</html>
